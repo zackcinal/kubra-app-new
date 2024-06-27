3b62b1bb81c7448913395bb35b81c959424a8ede
zackcinal <zekeriyacnl@gmail.com>
2024-06-27 10:04:41 -0400

    landing change

--- src/Screens/Landing/Landing.jsx
+++ src/Screens/Landing/Landing.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "./landing.css";
 import { Link } from "react-router-dom";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 function Landing() {
+
+  const images = [
+    {
+      original: 'https://i.imgur.com/L1sZ41b.jpeg',
+      originalHeight: '100%',
+      originalWidth: '100vw'
+    },
+    {
+      original: 'https://i.imgur.com/jcABApg.jpeg',
+      originalHeight: '100%',
+      originalWidth: '100vw'
+    },
+    // {
+    //   original: 'https://i.imgur.com/RkNbhR0.jpeg',
+    //   originalHeight: '100%',
+    //   originalWidth: '70vw'
+    // },
+    {
+      original: 'https://i.imgur.com/QM976FE.jpeg',
+      originalWidth: '100vw'
+    }
+  ]
+
+  class MyGallery extends React.Component {
+    render() {
+      return <ImageGallery items={images} showBullets={false} showThumbnails={false} showFullscreenButton={false} showPlayButton={false} autoPlay={true}/>;
+    }
+  }
 
   return (
     <>
@@ -10,22 +40,13 @@ function Landing() {
         <img src="https://i.imgur.com/yEUfhhW.png" className="logo-for-home" />
       </div>
       <div className="landing-main">
-        <h1 className="hi-there">Hi there!</h1>
         <h1 className="welcome-to">
           <span className="reveal-text">Welcome to</span>
           <br />
-          Pet Care by Kubra & Zack!
+          <span className="reveal-text">Pet Care by Kubra & Zack!</span>
         </h1>
       </div>
-      <div className="about-us">
-        <h2 className="h2-about-us">About Us</h2>
-        <p className="welcome-text">
-          We, Kubra & Zack, are a married couple who love dogs and have turned
-          that love into a dog walking business. We have been providing our
-          services in Downtown Jersey City for more than 2 years. Our services
-          include daily walks and boardings.
-        </p>
-      </div>
+      <MyGallery />
       <div className="buttons">
         <Link to="/signin">
           <button className="link-to-signin">Sign In</button>
@@ -39,3 +60,14 @@ function Landing() {
 }
 
 export default Landing;
+
+
+      {/* <div className="about-us">
+        <h2 className="h2-about-us">About Us</h2>
+        <p className="welcome-text">
+          We, Kubra & Zack, are a married couple who love dogs and have turned
+          that love into a dog walking business. We have been providing our
+          services in Downtown Jersey City for more than 2 years. Our services
+          include daily walks and boardings.
+        </p>
+      </div> */}
